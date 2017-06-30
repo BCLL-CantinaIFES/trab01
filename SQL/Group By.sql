b99c80032b28ce98b1d4bf6b438d721fb5eb6156
Luiz Antonio R. G <luizguzzo@gmail.com>
2017-06-30 12:55:34 -0300

    Erros corrigidos.

--- SQL/Group By.sql
+++ SQL/Group By.sql
@@ -4,22 +4,20 @@ inner join pessoa
 on (compra.id_pessoa = pessoa.id_pessoa)
 group by pessoa.nome;
 
-select pessoa.nome
-from pessoa
+select pessoa.nome as Pessoa, sum(produtos.preco_produto * item_compra.qnt_item) as "gastou em produtos" from pessoa
 inner join compra
 on (pessoa.id_pessoa = compra.id_pessoa)
 inner join item_compra
-on (compra.id_compra = item_compra.id_compra)
-inner join produto
-on (id_compra.id_item = id_produto)
+on (item_compra.id_compra = compra.id_compra)
+inner join produtos
+on (produtos.id_produto = item_compra.id_produto)
 group by pessoa.nome
-having produto.preco_produto > 2.00;
 
 select pessoa.nome, count(*)
 from compra
 inner join pessoa
 on (compra.id_pessoa = pessoa.id_pessoa)
-group by pessoa.nome
+group by pessoa.nome, pessoa.dat_nascimento
 having pessoa.dat_nascimento > '01/01/1996';
 
 select item_compra.qnt_item, pessoa.nome
@@ -28,18 +26,18 @@ inner join compra
 on (pessoa.id_pessoa = compra.id_pessoa)
 inner join item_compra
 on (compra.id_compra = item_compra.id_compra)
-inner join produto
-on (id_compra.id_item = id_produto)
+inner join produtos
+on (produtos.id_produto = item_compra.id_produto)
 group by item_compra.qnt_item, pessoa.nome
 having item_compra.qnt_item > 1;
 
-select produto.tipo_produto, pessoa.nome
+select produtos.tipo_produto, pessoa.nome
 from pessoa
 inner join compra
 on (pessoa.id_pessoa = compra.id_pessoa)
 inner join item_compra
 on (compra.id_compra = item_compra.id_compra)
-inner join produto
-on (id_compra.id_item = id_produto)
-group by produto.tipo_produto, pessoa.nome
-having produto.tipo_produto = 'Bebida';
+inner join produtos
+on (produtos.id_produto = item_compra.id_produto)
+group by produtos.tipo_produto, pessoa.nome
+having produtos.tipo_produto = 'Bebida';
